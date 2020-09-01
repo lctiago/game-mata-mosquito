45b5c630d3596dd5f849979677a2016c29434e65
lctiago <33674812+lctiago@users.noreply.github.com>
2020-09-01 13:11:31 -0300

    Adiciona primeiros arquivos do projeto

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+function updateBrowserWindowSize() {
+    browserWindowWidthInPixels = window.innerWidth
+    browserWindowHeightInPixels = window.innerHeight
+    console.log(browserWindowWidthInPixels + ',' + browserWindowHeightInPixels)
+}
+
+browserWindowWidthInPixels = 0
+browserWindowHeightInPixels = 0
