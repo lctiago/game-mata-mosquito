2445df757cbe8cbb4831da70534c379e5e67fc6b
lctiago <33674812+lctiago@users.noreply.github.com>
2020-09-01 15:22:14 -0300

    Adiciona surgimento de mosquitos em posições aleatórias na tela

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,45 @@
-function updateBrowserWindowSize() {
-    browserWindowWidthInPixels = window.innerWidth
-    browserWindowHeightInPixels = window.innerHeight
-    console.log(browserWindowWidthInPixels + ',' + browserWindowHeightInPixels)
+function gameStart() {
+    console.log('Inicializando o jogo')
+    defineInicialbrowserWindowSize()
+    console.log('Inicialização completa')
 }
 
-browserWindowWidthInPixels = 0
-browserWindowHeightInPixels = 0
+function defineInicialbrowserWindowSize() {
+    console.log('Inicializando tamanho da janela do browser')
+    browserWindowWidthInPixels = window.innerWidth
+    browserWindowHeightInPixels = window.innerHeight
+    console.log('Posição inicial=' + browserWindowWidthInPixels + ',' + browserWindowHeightInPixels)
+}
+
+function updateBrowserWindowSize() {
+    console.log('Atualizando tamanho da janela')
+    browserWindowWidthInPixels = window.innerWidth
+    browserWindowHeightInPixels = window.innerHeight
+    console.log('Novo tamanho da janela ' + browserWindowWidthInPixels + ',' + browserWindowHeightInPixels)
+}
+
+function defineMosquitoPosition(mosquito) {
+    console.log('Definindo posição aleatória para o novo mosquito')
+    var positionX = Math.floor(Math.random() * browserWindowWidthInPixels) - 50
+    var positionY = Math.floor(Math.random() * browserWindowHeightInPixels) - 50
+    positionX = positionX < 0 ? 0 : positionX
+    positionY = positionY < 0 ? 0 : positionY
+    mosquito.style.left = positionX + 'px'
+    mosquito.style.top = positionY + 'px'
+    mosquito.style.position = 'absolute'
+    console.log('Posição definida=' + positionX + ',' + positionY)
+}
+
+function newMosquito() {
+    console.log('Criando novo mosquito')
+    var newMosquito = document.createElement('img')
+    newMosquito.src = 'img/mosquito.png'
+    newMosquito.className = 'mosquito1'
+    defineMosquitoPosition(newMosquito)
+    document.body.appendChild(newMosquito)
+    console.log('Mosquito criado')
+}
+
+var browserWindowWidthInPixels = 0
+var browserWindowHeightInPixels = 0
+gameStart()
